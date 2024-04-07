17777c5e50fee3da68336804e81ac8cdca1bd9b5
jdrichards-pursuit <jdrpursuit@gmail.com>
2024-04-07 10:51:53 -0400

    update query

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,6 @@
 -- db/seed.sql
 \c firebase_full;
 
-INSERT INTO users (id,name, email, created_at, updated_at)
+INSERT INTO users (id,username, email, created_at, updated_at)
 VALUES 
 ('dJCId7hrpdghwM2rWbKcggvichv2','demo', 'demo@example.com', NOW(), NOW());

74bddac7b3d7e685a389566256bae40a4202eee9
jd richards <jdrichardstech@gmail.com>
2024-06-21 09:59:47 -0400

    add address to cors

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 // DEPENDENCIES
-const cors = require("cors");
-const express = require("express");
+const cors = require('cors')
+const express = require('express')
 
-const authController = require("./controllers/authController");
+const authController = require('./controllers/authController')
 
 // CONFIGURATION
-const app = express();
+const app = express()
 
 // MIDDLEWARE
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
-      "https://firebase-fullstack-auth-firestore.netlify.app",
+      'http://localhost:3000',
+      'https://firebase-auth-portfilio-client.netlify.app',
     ],
   })
-);
-app.use(express.json());
+)
+app.use(express.json())
 
-app.use("/api/auth", authController);
+app.use('/api/auth', authController)
 
 // ROUTES
-app.get("/", (_req, res) => {
-  res.send("Welcome to Firebase Backend Server");
-});
+app.get('/', (_req, res) => {
+  res.send('Welcome to Firebase Backend Server')
+})
 
 // 404 PAGE
-app.get("*", (_req, res) => {
-  res.status(404).send("Page not found");
-});
+app.get('*', (_req, res) => {
+  res.status(404).send('Page not found')
+})
 
 // EXPORT
-module.exports = app;
+module.exports = app

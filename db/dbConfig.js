9e89875b55d1dc4fa3e77e3b78d86d16b4dded14
jdrichards-pursuit <jdrpursuit@gmail.com>
2024-06-25 14:51:12 -0400

    fix dbconfig

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,16 +1,16 @@
-const pgp = require('pg-promise')()
-require('dotenv').config()
+const pgp = require("pg-promise")();
+require("dotenv").config();
 
-// const cn = {
-//   host: process.env.PG_HOST,
-//   port: process.env.PG_PORT,
-//   database: process.env.PG_DATABASE,
-//   user: process.env.PG_USER,
-//   password: process.env.PG_PASSWORD,
-// }
+const cn = {
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+};
 
-const cn = process.env.CONNECTION_STRING
+// const cn = process.env.CONNECTION_STRING
 
-const db = pgp(cn)
+const db = pgp(cn);
 
-module.exports = db
+module.exports = db;

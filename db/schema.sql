f656f679abcb01040d35e63dddf44127bf89e37f
Jaden <jadenglenn@pursuit.org>
2024-07-03 16:10:26 -0400

    schema start

--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,59 @@
--- db/schema.sql
-DROP DATABASE IF EXISTS debug_arena;
-CREATE DATABASE debug_arena;
+DROP DATABASE IF EXISTS h2h_backend;
+CREATE DATABASE h2h_backend;
 
-\c debug_arena;
+\c h2h_backend;
 
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY ,
+    id SERIAL PRIMARY KEY,
     uid VARCHAR(255),
     email VARCHAR(100),
     username VARCHAR(100),
     first_name VARCHAR(100),
     last_name VARCHAR(100),
     photo VARCHAR(100),
+    dob DATE,
+    user_wins INT,
+    user_losses INT,
+    matches_played INTEGER,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE team (
+    id INTEGER PRIMARY KEY,
+    team_name VARCHAR(20),
+    team_pic VARCHAR(230),
+    logo VARCHAR(230),
+    player1_id INTEGER,
+    player2_id INTEGER,
+    player3_id INTEGER,
+    player4_id INTEGER,
+    player5_id INTEGER,
+    captain_id INTEGER,
+    team_wins INTEGER,
+    team_loss INTEGER,
+    matches_played INTEGER,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (player1_id) REFERENCES users(id),
+    FOREIGN KEY (player2_id) REFERENCES users(id),
+    FOREIGN KEY (player3_id) REFERENCES users(id),
+    FOREIGN KEY (player4_id) REFERENCES users(id),
+    FOREIGN KEY (player5_id) REFERENCES users(id),
+    FOREIGN KEY (captain_id) REFERENCES users(id)
+);
+
+CREATE TABLE matches (
+    id INTEGER PRIMARY KEY,
+    team1_id INTEGER,
+    team2_id INTEGER,
+    address VARCHAR(30),
+    state VARCHAR(13),
+    city VARCHAR(20),
+    zip VARCHAR(10),
+    start_datetime DATE,
+    match_completed BOOLEAN DEFAULT FALSE,
+    match_winner INTEGER DEFAULT NULL,
+    match_loser INTEGER DEFAULT NULL
+);

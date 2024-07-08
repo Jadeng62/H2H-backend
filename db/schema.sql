0dfab259b26d23529f54cb834dcba494857dd17f
juligarc91 <julissagarcia@pursuit.org>
2024-07-08 04:34:18 -0400

    added cascade and removed commented out SELECT

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE h2h_backend;
 \c h2h_backend;
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    -- id SERIAL PRIMARY KEY,
     uid VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL,
@@ -20,34 +20,70 @@ CREATE TABLE users (
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
+-- CREATE TABLE team (
+--     id SERIAL PRIMARY KEY,
+--     team_name VARCHAR(100),
+--     team_pic VARCHAR(255),
+--     logo VARCHAR(255),
+--     player1_id VARCHAR(255),
+--     player2_id VARCHAR(255),
+--     player3_id VARCHAR(255),
+--     player4_id VARCHAR(255),
+--     player5_id VARCHAR(255),
+--     captain_id VARCHAR(255),
+--     team_wins INTEGER,
+--     team_loss INTEGER,
+--     matches_played INTEGER,
+--     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+--     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+--     FOREIGN KEY (player1_id) REFERENCES users(uid),
+--     FOREIGN KEY (player2_id) REFERENCES users(uid),
+--     FOREIGN KEY (player3_id) REFERENCES users(uid),
+--     FOREIGN KEY (player4_id) REFERENCES users(uid),
+--     FOREIGN KEY (player5_id) REFERENCES users(uid),
+--     FOREIGN KEY (captain_id) REFERENCES users(uid)
+-- );
+
+-- added CASCADE so that if a user in users table (referenced by uid) is deleted corresponding team records where columns reference that user will also be deleted automatically!!
 CREATE TABLE team (
     id SERIAL PRIMARY KEY,
     team_name VARCHAR(100),
     team_pic VARCHAR(255),
     logo VARCHAR(255),
-    player1_id VARCHAR(255),
-    player2_id VARCHAR(255),
-    player3_id VARCHAR(255),
-    player4_id VARCHAR(255),
-    player5_id VARCHAR(255),
-    captain_id VARCHAR(255),
+    player1_id VARCHAR(255) REFERENCES users(uid) ON DELETE CASCADE,
+    player2_id VARCHAR(255) REFERENCES users(uid) ON DELETE CASCADE,
+    player3_id VARCHAR(255) REFERENCES users(uid) ON DELETE CASCADE,
+    player4_id VARCHAR(255) REFERENCES users(uid) ON DELETE CASCADE,
+    player5_id VARCHAR(255) REFERENCES users(uid) ON DELETE CASCADE,
+    captain_id VARCHAR(255) REFERENCES users(uid) ON DELETE CASCADE,
     team_wins INTEGER,
     team_loss INTEGER,
     matches_played INTEGER,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (player1_id) REFERENCES users(uid),
-    FOREIGN KEY (player2_id) REFERENCES users(uid),
-    FOREIGN KEY (player3_id) REFERENCES users(uid),
-    FOREIGN KEY (player4_id) REFERENCES users(uid),
-    FOREIGN KEY (player5_id) REFERENCES users(uid),
-    FOREIGN KEY (captain_id) REFERENCES users(uid)
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+
+
+-- CREATE TABLE matches (
+--     id SERIAL PRIMARY KEY,
+--     team1_id INTEGER,
+--     team2_id INTEGER,
+--     address VARCHAR(100),
+--     state VARCHAR(20),
+--     city VARCHAR(20),
+--     zip VARCHAR(10),
+--     start_datetime TIMESTAMP WITH TIME ZONE,
+--     match_completed BOOLEAN DEFAULT FALSE,
+--     match_winner INTEGER DEFAULT NULL,
+--     match_loser INTEGER DEFAULT NULL,
+--     FOREIGN KEY (team1_id) REFERENCES team(id),
+--     FOREIGN KEY (team2_id) REFERENCES team(id)
+-- );
 
 CREATE TABLE matches (
     id SERIAL PRIMARY KEY,
-    team1_id INTEGER,
-    team2_id INTEGER,
+    team1_id INTEGER REFERENCES team(id) ON DELETE CASCADE,
+    team2_id INTEGER REFERENCES team(id) ON DELETE CASCADE,
     address VARCHAR(100),
     state VARCHAR(20),
     city VARCHAR(20),
@@ -55,7 +91,5 @@ CREATE TABLE matches (
     start_datetime TIMESTAMP WITH TIME ZONE,
     match_completed BOOLEAN DEFAULT FALSE,
     match_winner INTEGER DEFAULT NULL,
-    match_loser INTEGER DEFAULT NULL,
-    FOREIGN KEY (team1_id) REFERENCES team(id),
-    FOREIGN KEY (team2_id) REFERENCES team(id)
+    match_loser INTEGER DEFAULT NULL
 );

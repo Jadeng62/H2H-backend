118bb522ab052ed8b447a319e65b81a7771a1a8d
juligarc91 <julissagarcia@pursuit.org>
2024-07-08 05:01:26 -0400

    adjusted tables

--- db/seed.sql
+++ db/seed.sql
@@ -13,18 +13,19 @@
 -- to query users from firebase use uid (not id)
 
 -- Insert users
-INSERT INTO users (id, uid, email, password, username, first_name, last_name, photo, dob)
+INSERT INTO users (uid, email, password, username, first_name, last_name, photo, dob, user_wins, user_losses, matches_played, created_at, updated_at)
 VALUES
-    (1, 'uid1', 'user1@example.com', 'password1', 'user1', 'John', 'Doe', 'user1.jpg', '1990-01-01'),
-    (2, 'uid2', 'user2@example.com', 'password2', 'user2', 'Jane', 'Smith', 'user2.jpg', '1995-05-15'),
-    (3, 'uid3', 'user3@example.com', 'password3', 'user3', 'Michael', 'Johnson', 'user3.jpg', '1988-09-30');
+    (1, 'user1@example.com', 'password1', 'user1', 'John', 'Doe', 'user1.jpg', '1990-01-01', 0, 0, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (2, 'user2@example.com', 'password2', 'user2', 'Jane', 'Smith', 'user2.jpg', '1995-05-15', 0, 0, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (3, 'user3@example.com', 'password3', 'user3', 'Michael', 'Johnson', 'user3.jpg', '1988-09-30', 0, 0, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
-INSERT INTO team (id, team_name, team_pic, logo, player1_id, player2_id, player3_id, captain_id, team_wins, team_loss, matches_played)
+INSERT INTO team (id, team_name, team_pic, logo, player1_id, player2_id, player3_id, captain_id, team_wins, team_loss, matches_played, created_at, updated_at)
 VALUES
-    (1, 'Team A', 'team_a_pic.jpg', 'team_a_logo.jpg', 'uid1', 'uid2', 'uid3', 'uid1', 2, 1, 3),
-    (2, 'Team B', 'team_b_pic.jpg', 'team_b_logo.jpg', 'uid2', 'uid3', 'uid1', 'uid2', 1, 2, 3);
+    (1, 'Team A', 'team_a_pic.jpg', 'team_a_logo.jpg', 1, 2, 3, 1, 2, 1, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (2, 'Team B', 'team_b_pic.jpg', 'team_b_logo.jpg', 2, 3, 1, 2, 1, 2, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO matches (id, team1_id, team2_id, address, state, city, zip, start_datetime, match_completed, match_winner, match_loser)
 VALUES
     (1, 1, 2, '123 Main St', 'CA', 'Los Angeles', '90001', '2024-07-08 14:00:00', TRUE, 1, 2),
     (2, 2, 1, '456 Oak Ave', 'NY', 'New York', '10001', '2024-07-09 15:30:00', TRUE, 2, 1);
+-- works if inserted using psql terminal, but for some reason not if using npm run db:seed

2cfdb1e825d185f71c238d14dd57cc81d4873f2e
Carlitos <carlitosdutan@pursuit.org>
2024-07-11 16:25:47 -0400

    fixed seed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,26 +1,26 @@
-const express = require('express')
-const auth = express.Router()
-const authMiddleware = require('../middlewares/authMiddleware')
-const { createNewUser, findUserByUID } = require('../queries/users.js')
+const express = require("express");
+const auth = express.Router();
+const authMiddleware = require("../middlewares/authMiddleware");
+const { createNewUser, findUserByUID } = require("../queries/users.js");
 
-auth.post('/register', async (req, res) => {
-  const newUser = await createNewUser(req.body)
+auth.post("/register", async (req, res) => {
+  const newUser = await createNewUser(req.body);
 
   if (newUser) {
-    res.status(201).json(newUser)
+    res.status(201).json(newUser);
   } else {
-    res.status(500).json({ error: 'Error creating user' })
+    res.status(500).json({ error: "Error creating user" });
   }
-})
+});
 
-auth.get('/user/:uid', authMiddleware, async (req, res) => {
-  const { uid } = req.params
-  const user = await findUserByUID(uid)
+auth.get("/user/:uid", authMiddleware, async (req, res) => {
+  const { uid } = req.params;
+  const user = await findUserByUID(uid);
   if (user) {
-    res.status(200).json(user)
+    res.status(200).json(user);
   } else {
-    res.status(500).json({ error: 'Error fetching user' })
+    res.status(500).json({ error: "Error fetching user" });
   }
-})
+});
 
-module.exports = auth
+module.exports = auth;

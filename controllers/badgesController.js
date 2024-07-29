7a9a6d3e0e3f1368c36fe4430cc961b2983524b1
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-07-29 12:46:39 -0400

    added error handling for debuging routes in badges later

--- controllers/badgesController.js
+++ controllers/badgesController.js
@@ -13,9 +13,13 @@ badges.post("/teams/:teamId/badges/:badgeId", async (req, res) => {
   const { teamId, badgeId } = req.params;
   try {
     const newBadge = await addBadgeToTeam(teamId, badgeId);
-    res.status(201).json(newBadge);
+    if (newBadge) {
+      res.status(201).json(newBadge);
+    } else {
+      res.status(404).json({ error: "Badge could not be created" });
+    }
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
@@ -24,9 +28,15 @@ badges.get("/teams/:teamId", async (req, res) => {
   const { teamId } = req.params;
   try {
     const badges = await getBadgesByTeamId(teamId);
-    res.status(200).json(badges);
+    if (badges) {
+      res.status(200).json(badges);
+    } else {
+      res
+        .status(404)
+        .json({ error: "Badges with specified team ID could not be found" });
+    }
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
@@ -35,9 +45,15 @@ badges.get("/:badgeId", async (req, res) => {
   const { badgeId } = req.params;
   try {
     const badge = await getBadgeById(badgeId);
-    res.status(200).json(badge);
+    if (badge) {
+      res.status(200).json(badge);
+    } else {
+      res
+        .status(404)
+        .json({ error: "Badge with specified ID could not be found" });
+    }
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
@@ -45,9 +61,13 @@ badges.get("/:badgeId", async (req, res) => {
 badges.get("/", async (req, res) => {
   try {
     const badges = await getAllBadges();
-    res.status(200).json(badges);
+    if (badges) {
+      res.status(200).json(badges);
+    } else {
+      res.status(404).json({ error: "No badges found" });
+    }
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
